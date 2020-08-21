37b020f8cf86ffeea5a6d2b94d7ff0e8c39ec109
LeonidWebDev <55695468+LeonidWebDev@users.noreply.github.com>
2020-08-22 01:19:07 +0300

    addTestForPower_up

--- hero_spec.rb
+++ hero_spec.rb
@@ -1,4 +1,15 @@
 require './hero'
 
 describe Hero do
+	
+	it "has a capitalized name" do
+		hero = Hero.new 'don'
+		expect(hero.name).to eq "Don"
+	end
+
+	it "can power up" do
+		hero = Hero.new "don"
+		expect(hero.power_up).to eq 110
+	end
+
 end
