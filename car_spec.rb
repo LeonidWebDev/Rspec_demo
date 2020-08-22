ccd4b4059c684fc72b7a1203ffdae8520f53ab42
LeonidWebDev <55695468+LeonidWebDev@users.noreply.github.com>
2020-08-22 14:26:09 +0300

    addTestForCar

---
+++ car_spec.rb
@@ -0,0 +1,21 @@
+require "./car"
+
+describe Car do
+
+	it "must return range" do
+
+	# arrange  подготовка
+
+		car = Car.new
+
+	# act действие
+
+	        car.add_fuel 10
+
+	# assert
+
+		expect(car.range).to eq 200  
+		
+	end
+	
+end
