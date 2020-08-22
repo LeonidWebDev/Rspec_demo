246395ad369b49a2f4415beecc8213e3bb5c205e
LeonidWebDev <55695468+LeonidWebDev@users.noreply.github.com>
2020-08-22 13:17:03 +0300

    addInfo_test

--- hero_spec.rb
+++ hero_spec.rb
@@ -12,4 +12,15 @@ describe Hero do
 		expect(hero.power_up).to eq 110
 	end
 
+	it "can power down" do
+		hero = Hero.new "don"
+		expect(hero.power_down).to eq 90
+	end
+
+	it "displays full hero info" do
+		hero = Hero.new "don"
+		expect(hero.hero_info).to eq "Don has a health of 100"
+	end
+	
+
 end
